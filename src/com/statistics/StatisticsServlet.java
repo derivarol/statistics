e3cf8985db8523b70b46d455459d878562462f0a
M. deRivarol <de.rivarol@gmail.com>
2012-04-21 20:37:00 +0300

    first commitment of just created project: statistics;

---
+++ src/com/statistics/StatisticsServlet.java
@@ -0,0 +1,13 @@
+package com.statistics;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class StatisticsServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
